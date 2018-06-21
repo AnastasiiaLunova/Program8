807ed9cfa62f8043ec7c3a0cb0d2548cb4a3821c
malwalanda <malwalanda@gmail.com>
2018-06-21 10:36:15 +0300

    Create test ckeleton

--- Program8/src/test/org/itstep/ModelTest.java
+++ Program8/src/test/org/itstep/ModelTest.java
@@ -7,9 +7,55 @@ import static org.junit.Assert.*;
 
 public class ModelTest {
     @Test
-    public void test() {
+    public void inputWordByScanerWord1() {
         Model model = new Model();
+        String runTest = model.inputWordByScanerWord1("Anastasiia");
+        assertEquals("Anastasiia", runTest);
+    }
 
-        assertEquals("test", "yyyyy");
+    @Test
+    public void inputWordByScanerWord1v2(){
+        Model model = new Model();
+        String runTest = model.inputWordByScanerWord1("Katya");
+        assertNull(runTest);
+    }
+
+    @Test
+    public void inputWordByScanerWord2(){
+        Model model = new Model();
+        String runTest = model.inputWordByScanerWord2("Lunova");
+        assertEquals("Lunova", runTest);
+    }
+
+    @Test
+    public void inputWordByScanerWord2v2(){
+        Model model = new Model();
+        String runTest = model.inputWordByScanerWord2("Ivanova");
+        assertNull(runTest);
+    }
+
+    @Test
+    public void inputWordByScanerNumber(){
+        Model model = new Model();
+        int runTest = model.inputWordByScanerNumber(8);
+        assertEquals(8,runTest);
+    }
+
+    @Test
+    public void inputWordByScanerNumberV2(){
+        Model model = new Model();
+        int runTest = model.inputWordByScanerNumber(10);
+        assertEquals(0, runTest);
+    }
+
+    @Test
+    public void writeSentence(){
+        Model model = new Model();
+        String runTest = model.writeSentence("Anastasiia", "Lunova", 8);
+        assertEquals("Anastasiia " + " Lunova " +8,runTest);
     }
 }
+
+
+
+
